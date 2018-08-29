7f0e0ad2b5ac89f38c042d306ab6687a19fb69a9
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2018-08-29 15:30:59 +0000

    Update Slugwrath.py

--- Slugwrath.py
+++ Slugwrath.py
@@ -1,2 +1,11 @@
 import discord
-form discord.ext import commands
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = 'immortal ')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+    
+
+
